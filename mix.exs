6323ee0551e3d3ade124ef6823679f76601c13f6
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-03-31 13:17:48 +0300

    Bump application version in mix.exs to 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoContract.MixProject do
   def project do
     [
       app: :ecto_contract,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
